c5d7889fc8b0ebbd03bfd6b31f74bd1453c1c4dc
saeshukarthika <saeshukarthika@gmail.com>
2024-02-02 11:43:35 -0600

    Update helloworld

---
+++ Experiments/demo1_saeshu/helloworld/src/main/java/coms309/Application.java
@@ -0,0 +1,19 @@
+package coms309;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CyStudy Experiment 1.
+ * 
+ * @author Saeshu Karthika
+ */
+
+@SpringBootApplication
+public class Application {
+	
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
