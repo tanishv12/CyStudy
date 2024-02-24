1ba608c4763b0a5c1c7bd186ddb812deb5f84da5
saeshukarthika <saeshukarthika@gmail.com>
2024-02-24 13:49:42 -0600

    update user

--- Experiments/demo1_saeshu/test/src/main/java/coms309/roundtrip/test/controllers/UserController.java
+++ Experiments/demo1_saeshu/test/src/main/java/coms309/roundtrip/test/controllers/UserController.java
@@ -1,9 +1,12 @@
 package coms309.roundtrip.test.controllers;
 
+import coms309.roundtrip.test.model.User;
 import coms309.roundtrip.test.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -11,8 +14,39 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+    
+    @GetMapping("/users")
+    List<User> getallUsers(){ return userRepository.findAll();}
 
-    @GetMapping("user/all")
-    List
+    @GetMapping("/users/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping(path = "/users")
+    String createUser(User user){
+        if (user == null)
+            return failure;
+        userRepository.save(user);
+        return success;
+    }
+
+    @PutMapping("/users/{id}")
+    Optional<User> updateUser(@PathVariable Long id, @RequestBody User request){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user == null) {
+            throw new RuntimeException("user id does not exist");
+        }
+        else if (user.get().getId()!= id){
+            throw new RuntimeException("path variable id does not match User request id");
+        }
+
+        userRepository.save(request);
+        return userRepository.findById(id);
+    }
+
 }
 
--- Experiments/demo1_saeshu/test/src/main/java/coms309/roundtrip/test/model/User.java
+++ Experiments/demo1_saeshu/test/src/main/java/coms309/roundtrip/test/model/User.java
@@ -12,71 +12,52 @@ public class User {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
-    private String userName;
     private String emailId;
+    private String password;
     private boolean ifActive;
-
-    /*
-     * @OneToOne creates a relation between the current entity/table(Laptop) with the entity/table defined below it(User)
-     * cascade is responsible propagating all changes, even to children of the class Eg: changes made to laptop within a user object will be reflected
-     * in the database (more info : https://www.baeldung.com/jpa-cascade-types)
-     * @JoinColumn defines the ownership of the foreign key i.e. the user table will have a field called laptop_id
-     */
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "laptop_id")
-    private Laptop laptop;
-
-    public User(String name, String emailId) {
-        this.name = name;
-        this.emailId = emailId;
-        this.ifActive = true;
-    }
 
     public User() {
     }
 
-    // =============================== Getters and Setters for each field ================================== //
-
-    public int getId(){
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getEmailId(){
+    public String getEmailId() {
         return emailId;
     }
 
-    public void setEmailId(String emailId){
+    public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
 
-    public boolean getIsActive(){
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIfActive() {
         return ifActive;
     }
 
-    public void setIfActive(boolean ifActive){
+    public void setIfActive(boolean ifActive) {
         this.ifActive = ifActive;
     }
-
-    public Laptop getLaptop(){
-        return laptop;
-    }
-
-    public void setLaptop(Laptop laptop){
-        this.laptop = laptop;
-    }
-
 }
