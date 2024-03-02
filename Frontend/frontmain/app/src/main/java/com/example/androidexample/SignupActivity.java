e8ea9d1c598795facec308d8ce0e28c937a24f8e
Gavin_McClure <grm04@iastate.edu>
2024-03-02 08:18:56 -0600

    added login page

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -3,12 +3,25 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText signupName, signupEmail, signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        signupName = findViewById(R.id.signup_name);
+        signupEmail = findViewById(R.id.signup_email);
+        signupUsername = findViewById(R.id.signup_username);
+        signupPassword = findViewById(R.id.signup_password);
     }
 }
