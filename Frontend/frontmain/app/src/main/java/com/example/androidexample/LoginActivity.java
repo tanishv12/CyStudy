cc2784e2a82dd8c6e986ff0a2e8803f752167197
Gavin_McClure <grm04@iastate.edu>
2024-03-02 09:14:27 -0600

    All buttons work

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -3,12 +3,60 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginUsername, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+        loginButton = findViewById(R.id.login_button);
+    }
+
+    public Boolean validateUsername()
+    {
+        String val = loginUsername.getText().toString();
+        if(val.isEmpty())
+        {
+            loginUsername.setError("Username cannot be empty");
+            return false;
+        }
+        else
+        {
+            loginUsername.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword()
+    {
+        String val = loginPassword.getText().toString();
+        if(val.isEmpty())
+        {
+            loginPassword.setError("Password cannot be empty");
+            return false;
+        }
+        else
+        {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser()
+    {
+        String userUsername = loginUsername.getText().toString().trim();
+        String userPassword = loginPassword.getText().toString().trim();
     }
 }
--- Frontend/frontmain/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -2,10 +2,14 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -23,5 +27,33 @@ public class SignupActivity extends AppCompatActivity {
         signupEmail = findViewById(R.id.signup_email);
         signupUsername = findViewById(R.id.signup_username);
         signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                HelperClass helperClass = new HelperClass(name, email, username, password);
+                //reference.child(name).setValue(helperClass);
+                Toast.makeText(SignupActivity.this, "You have signed up successfully", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
