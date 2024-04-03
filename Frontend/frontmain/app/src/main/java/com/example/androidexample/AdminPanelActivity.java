17cb269918f6d948680bd34c9b2ffbfe63fc285e
Gavin_McClure <grm04@iastate.edu>
2024-04-03 13:21:55 -0500

    finished class construction

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/AdminPanelActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/AdminPanelActivity.java
@@ -2,32 +2,12 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-import org.java_websocket.handshake.ServerHandshake;
-
-public class AdminPanelActivity extends AppCompatActivity{
+public class AdminPanelActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
     }
