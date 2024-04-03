e487ebba320c7b0a1da808ab31459fe5b06aec86
Tanish Visanagiri <tanishv12@gmail.com>
2024-04-03 01:46:02 -0500

    finished developing functionality of rating and reviewing system - make it work with backend

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/RatingReview.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/RatingReview.java
@@ -3,12 +3,37 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
-public class RatingReview extends AppCompatActivity {
+public class RatingReview extends AppCompatActivity
+{
+    private RatingBar ratingBar;
+    private Button submitrate;
+
+    private EditText reviewTxt;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_review);
+        ratingBar = findViewById(R.id.ratingBar);
+        submitrate = findViewById(R.id.submitRate);
+        reviewTxt = findViewById(R.id.ReviewText);
+
+        submitrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), s + " Stars", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
