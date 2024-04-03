858e89bafcb19364ed87f687f24485a9f1262d8d
Gavin_McClure <grm04@iastate.edu>
2024-04-03 18:35:09 -0500

    added button functionality

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/AdminPanelActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/AdminPanelActivity.java
@@ -2,7 +2,10 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminPanelActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class AdminPanelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
+
+        Button chatLogs, groupManagement, userManagement, addResources, returnHome;
+
+        chatLogs = findViewById(R.id.chat_logs);
+        groupManagement = findViewById(R.id.group_management);
+        userManagement = findViewById(R.id.user_management);
+        addResources = findViewById(R.id.add_resources);
+        returnHome = findViewById(R.id.return_home);
+
+        chatLogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        returnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminPanelActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
