db7ee1b931458208e1e4788dcbecd60d2b5f6c4e
Tanish Visanagiri <tanishv12@gmail.com>
2024-04-03 01:32:19 -0500

    developing rate and review screen

---
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/RatingReview.java
@@ -0,0 +1,14 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RatingReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_review);
+    }
+}
