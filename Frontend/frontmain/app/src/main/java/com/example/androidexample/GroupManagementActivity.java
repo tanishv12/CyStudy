b1a14d95408aef74aa06f031f2253709010ed98d
Gavin_McClure <grm04@iastate.edu>
2024-04-05 01:33:46 -0500

    all xml elements initialized

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupManagementActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupManagementActivity.java
@@ -3,12 +3,25 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GroupManagementActivity extends AppCompatActivity {
+
+    EditText groupName;
+    Button createGroup, readGroup, updateGroup, deleteGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_management);
+
+        groupName = findViewById(R.id.group_name);
+        createGroup = findViewById(R.id.create_group);
+        readGroup = findViewById(R.id.read_group);
+        updateGroup = findViewById(R.id.update_group);
+        deleteGroup = findViewById(R.id.delete_group);
+
+
     }
 }
