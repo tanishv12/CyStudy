5d09687b0244916bf06b7ae81e0077365636abe3
Gavin_McClure <grm04@iastate.edu>
2024-04-05 19:32:44 -0500

    button functionality

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupManagementActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupManagementActivity.java
@@ -7,12 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONObject;
 
@@ -21,31 +24,47 @@ import java.util.Map;
 
 public class GroupManagementActivity extends AppCompatActivity {
 
-    EditText groupName;
-    Button createGroup, readGroup, updateGroup, deleteGroup;
+    private EditText groupName;
+    private TextView chat;
+    private Button create_group, read_group, update_group, delete_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_management);
 
-        groupName = findViewById(R.id.group_name);
-        createGroup = findViewById(R.id.create_group);
-        readGroup = findViewById(R.id.read_group);
-        updateGroup = findViewById(R.id.update_group);
-        deleteGroup = findViewById(R.id.delete_group);
+        chat = findViewById(R.id.all_chat);
+        groupName = findViewById(R.id.groupName);
+        create_group = findViewById(R.id.createGroup);
+        read_group = findViewById(R.id.readGroup);
+        update_group = findViewById(R.id.updateGroup);
+        delete_group = findViewById(R.id.deleteGroup);
 
-        createGroup.setOnClickListener(new View.OnClickListener() {
+        create_group.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //Map to create group screen (?)
             }
         });
 
-        deleteGroup.setOnClickListener(new View.OnClickListener() {
+        delete_group.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 deleteRequest();
+            }
+        });
+
+        read_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRequest();
+            }
+        });
+
+        update_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                putRequest();
             }
         });
         //Most of the methods are in StudyGroupFragment. Copy over from there.
@@ -54,7 +73,7 @@ public class GroupManagementActivity extends AppCompatActivity {
     /**
      * Deletes study group that the user is currently in
      *
-     * IMPORTANT: Must change to deletes the group using given name
+     * IMPORTANT: Must change url to delete the group using given name
      */
     private void deleteRequest()
     {
@@ -91,7 +110,7 @@ public class GroupManagementActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response)
                     {
-
+                        chat.setText(response.toString());
                         Log.e("Response Entered",response.toString());
 
                     }
@@ -102,8 +121,129 @@ public class GroupManagementActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error)
                     {
                         Log.e("Error Response", error.toString());
-//                        Toast.makeText(StudyGroupFragment.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(GroupManagementActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
 
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+                //                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                //                params.put("param1", "value1");
+                //                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
+    /**
+     * Retrieves study group that the user is in
+     */
+    private void getRequest()
+    {
+        String url = "http://coms-309-016.class.las.iastate.edu:8080/groups/all";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Display the first 500 characters of the response string.
+                        // String response can be converted to JSONObject via
+                        // JSONObject object = new JSONObject(response);
+                        chat.setText(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+                //                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                //                params.put("param1", "value1");
+                //                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }
+
+    /**
+     * Updates study group name that the user is in
+     */
+    private void putRequest()
+    {
+        String url = "http://coms-309-016.class.las.iastate.edu:8080/groups/update";
+        // Convert input to JSONObject
+        JSONObject putBody = null;
+
+        try
+        {
+            Log.e("Try Entered","oisafuhgiureshg");
+            // etRequest should contain a JSON object string as your POST body
+            // similar to what you would have in POSTMAN-body field
+            // and the fields should match with the object structure of @RequestBody on sb
+            putBody = new JSONObject();
+            putBody.put("groupName", groupName.getText().toString());
+            Log.e("what is putbody",putBody.toString());
+            Log.e("Try BLAH","oisafuhgiureshg");
+            url += "/" + "8";
+            Log.e("What is url",url.toString());
+////                    + "/" + loginPassword.getText().toString();
+        }
+        catch (Exception e)
+        {
+            Log.e("Catch Entered","wkerufhieuwhf");
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                putBody,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Log.e("Response Entered",response.toString());
+                        chat.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.e("Error Response", error.toString());
+                        Toast.makeText(GroupManagementActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
                     }
                 }
         ){
