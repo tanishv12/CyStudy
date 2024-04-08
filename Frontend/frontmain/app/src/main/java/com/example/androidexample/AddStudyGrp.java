df90ce4b57f119054a6e230486d83b560fdf3946
Tanish Visanagiri <tanishv12@gmail.com>
2024-04-07 19:34:26 -0500

    posting newly created grps to the database

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/AddStudyGrp.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/AddStudyGrp.java
@@ -14,6 +14,17 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddStudyGrp extends AppCompatActivity {
 
     private EditText numUsers;
@@ -21,6 +32,8 @@ public class AddStudyGrp extends AppCompatActivity {
     private int count = 2;
     private ImageButton increaseUser;
     private ImageButton decreaseUser;
+
+    private String user;
 
     private AutoCompleteTextView cName;
     private Button createGrp;
@@ -44,6 +57,8 @@ public class AddStudyGrp extends AppCompatActivity {
         cName = findViewById(R.id.courseName);
 
         groupText = findViewById(R.id.groupName);
+
+        user = UsernameSingleton.getInstance().getUserName();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, COURSES);
@@ -77,6 +92,7 @@ public class AddStudyGrp extends AppCompatActivity {
                     returnIntent.putExtra("groupName", gName);//Essentially should be what the user gives
                     setResult(Activity.RESULT_OK, returnIntent);
                     Log.e("groupName", "group: " + gName);
+                    postRequest();
                     finish();
                 }
             }
@@ -108,7 +124,73 @@ public class AddStudyGrp extends AppCompatActivity {
                 numUsers.setText("" + count);
             }
         });
+    }
+    private void postRequest()
+    {
+        String url = "http://coms-309-016.class.las.iastate.edu:8080/groups/post";
+        // Convert input to JSONObject
+        JSONObject postBody = null;
 
+        try
+        {
+            Log.e("Try Entered","oisafuhgiureshg");
+            // etRequest should contain a JSON object string as your POST body
+            // similar to what you would have in POSTMAN-body field
+            // and the fields should match with the object structure of @RequestBody on sb
+            postBody = new JSONObject();
+            Log.e("JSON Created","Json was created bla bla");
+            postBody.put("studyGroup", groupText.getText().toString());
+            url += "/" + user + "groupText.getText().toString()";
+////                    + "/" + loginPassword.getText().toString();
+        }
+        catch (Exception e)
+        {
+            Log.e("Catch Entered","wkerufhieuwhf");
+            e.printStackTrace();
+        }
 
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                postBody,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Log.e("Response Entered",response.toString());
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.e("Error Response", error.toString());
+//                        Toast.makeText(StudyGroupFragment.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+                //                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                //                params.put("param1", "value1");
+                //                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
 }
