6a6622ec820bf36c49fb86eef777670309d3505c
grm04 <grm04@iastate.edu>
2024-04-23 15:01:07 -0500

    added screen

---
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidexample;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import org.java_websocket.handshake.ServerHandshake;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
