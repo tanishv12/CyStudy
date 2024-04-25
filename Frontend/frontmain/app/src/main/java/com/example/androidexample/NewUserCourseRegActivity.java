dd7797fd95287f4338ff4a11edc09266446aff94
Gavin_McClure <grm04@iastate.edu>
2024-04-24 21:53:15 -0500

    created new class without any bugs

---
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/NewUserCourseRegActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NewUserCourseRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
