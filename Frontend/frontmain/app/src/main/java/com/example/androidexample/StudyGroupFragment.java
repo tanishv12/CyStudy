1e1821c50db17a753b7b1ae84f6e5256bce82175
Gavin_McClure <grm04@iastate.edu>
2024-04-25 19:40:03 -0500

    Revert "deleting basic button"
    This reverts commit d72014fc756a4d5d93d5bf321810b9b51fe31dbb.

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/StudyGroupFragment.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/StudyGroupFragment.java
@@ -331,6 +331,7 @@ public class StudyGroupFragment extends AppCompatActivity implements WebSocketLi
 
 
         addGrp = findViewById(R.id.addGroup);
+        studyGroupsToMessages = findViewById(R.id.toMessages);
         optionBtn = findViewById(R.id.optionButton);
 //        getButton = findViewById(R.id.getBUTTON);
         gresponse = findViewById(R.id.getresponse);
@@ -425,6 +426,22 @@ public class StudyGroupFragment extends AppCompatActivity implements WebSocketLi
 //                putRequest();
 //            }
 //        });
+
+
+
+
+
+        /**
+         * Redirects user to groupchat activity
+         */
+        studyGroupsToMessages.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StudyGroupFragment.this, MessageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //group_id   course_id
