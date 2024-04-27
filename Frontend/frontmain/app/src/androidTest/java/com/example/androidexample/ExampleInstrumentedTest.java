85f6f8aa6edd8ef9818078227bbaefc5cca31cf6
grm04 <grm04@iastate.edu>
2024-04-26 19:48:28 -0500

    tests

--- Frontend/frontmain/app/src/androidTest/java/com/example/androidexample/ExampleInstrumentedTest.java
+++ Frontend/frontmain/app/src/androidTest/java/com/example/androidexample/ExampleInstrumentedTest.java
@@ -1,10 +1,21 @@
 package com.example.androidexample;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.app.Activity;
 import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.ActivityTestRule;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,12 +26,16 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<SignupActivity> activityRule = new ActivityScenarioRule<>(SignupActivity.class);
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.androidexample", appContext.getPackageName());
+        activityRule.getScenario().onActivity(activity -> {
+            onView(withId(R.id.signup_button)).perform(click());
+            onView(withId(R.id.bottomnavbar)).check(matches(isDisplayed()));
+        });
     }
 }
