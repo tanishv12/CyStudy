09b909af54c455c331cd3a0502ef2e771e7c52fc
Gavin_McClure <grm04@iastate.edu>
2024-04-27 19:13:06 -0500

    adding dynamic course implementation

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/NewCourseRegActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/NewCourseRegActivity.java
@@ -1,14 +1,30 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class NewCourseRegActivity extends AppCompatActivity {
+
+    public int convertDpToPixels(float dp, Context context)
+    {
+        return (int) (dp * getResources().getDisplayMetrics().density);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_course_reg);
+    }
+
+    private CardView createCard(String courseDept, String courseCode)
+    {
+        CardView cardView = new CardView(this);
+        cardView.setCardElevation(convertDpToPixels(4, this));
+        cardView.setRadius(convertDpToPixels(4, this));
+
+
     }
 }
