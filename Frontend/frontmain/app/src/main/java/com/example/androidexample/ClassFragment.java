685de31374efd48a6e3b84f2afdfadd32195b124
Tanish Visanagiri <tanishv12@gmail.com>
2024-04-28 02:20:35 -0500

    developing and implementing join group screen

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/ClassFragment.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/ClassFragment.java
@@ -12,67 +12,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClassFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-
-
 public class ClassFragment extends AppCompatActivity {
-    Button b1;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private Button enter166;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ClassFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClassFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClassFragment newInstance(String param1, String param2) {
-        ClassFragment fragment = new ClassFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private Button enterCourse;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_class);
 
-        enter166 = findViewById(R.id.math166);
+        enterCourse = findViewById(R.id.enterCourse);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavbar);
         bottomNavigationView.setSelectedItemId(R.id.Classes);
 
 
-        enter166.setOnClickListener(new View.OnClickListener() {
+        enterCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-//                Intent intent = new Intent(ClassFragment.this, Math166.class);
-//                startActivity(intent);
+                Intent intent = new Intent(ClassFragment.this, JoinStudyGroupCourses.class);
+                startActivity(intent);
             }
         });
 
--- Frontend/frontmain/app/src/main/java/com/example/androidexample/JoinStudyGroupCourses.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/JoinStudyGroupCourses.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Math166 extends AppCompatActivity
+public class JoinStudyGroupCourses extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        setContentView(R.layout.activity_math166);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_study_group_courses);
     }
 }
