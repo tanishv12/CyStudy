e68b7d907fc6c3831bf288147d22c5c1c9465f3b
Gavin_McClure <grm04@iastate.edu>
2024-04-29 03:06:28 -0500

    temp changes

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/ProfileActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidexample;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView name, email, username, password;
 
     /**
      * Creates home page UI
@@ -18,9 +22,23 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_profile);
+        super.onCreate(savedInstanceState);
+
+        name=findViewById(R.id.signup_name_textView);
+        email=findViewById(R.id.signup_email_textView);
+        username=findViewById(R.id.signup_username_textView);
+        password=findViewById(R.id.signup_password_textView);
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+
+        //Navigation Bar Code Start
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavbar);
         bottomNavigationView.setSelectedItemId(R.id.Profile);
-
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if(item.getItemId() == R.id.Home)
             {
@@ -43,9 +61,22 @@ public class ProfileActivity extends AppCompatActivity {
             }
             return false;
         });
-        super.onCreate(savedInstanceState);
+        //Navigation Bar Code End
 
+    }
+    // Method to show the custom dialog
+    private void showCustomDialog() {
+        // Inflate the custom dialog layout
+        View dialogView = getLayoutInflater().inflate(R.layout.update_info_activity, null);
 
+        // Create a dialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
 
+        // Create the dialog
+        AlertDialog dialog = builder.create();
+
+        // Show the dialog
+        dialog.show();
     }
 }
