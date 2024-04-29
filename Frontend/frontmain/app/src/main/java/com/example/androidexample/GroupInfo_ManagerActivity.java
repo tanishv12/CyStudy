60be2f040dc66b69ec78a4b4879ea754a9416b01
Tanish Visanagiri <tanishv12@gmail.com>
2024-04-29 02:55:57 -0500

    developed some of the manager features from the group info screen

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupInfo_ManagerActivity.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/GroupInfo_ManagerActivity.java
@@ -3,10 +3,15 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class GroupInfo_ManagerActivity extends AppCompatActivity {
 
@@ -26,10 +31,62 @@ public class GroupInfo_ManagerActivity extends AppCompatActivity {
 
     private Button leaveGroupBtn;
     private Button doneButton;
+
+
+    private void optionsDialog(Context c)
+    {
+        LayoutInflater inflater = LayoutInflater.from(c);
+        View dialogView = inflater.inflate(R.layout.group_options_manager, null);
+
+        BottomSheetDialog dialog = new BottomSheetDialog(c);
+        dialog.setTitle("Options");
+        dialog.setContentView(dialogView);
+        dialog.create();
+
+        leaveGroupBtn = dialogView.findViewById(R.id.leaveGroup);
+        editGroupBtn = dialogView.findViewById(R.id.editGrpName);
+        doneButton = dialogView.findViewById(R.id.doneBtn);
+
+        leaveGroupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        editGroupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                dialog.dismiss(); // Close the dialog if needed
+            }
+        });
+
+        dialog.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info_manager);
+        optionsGrpBtn = findViewById(R.id.options);
+        optionsGrpBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                optionsDialog(GroupInfo_ManagerActivity.this);
+            }
+        });
     }
 }
