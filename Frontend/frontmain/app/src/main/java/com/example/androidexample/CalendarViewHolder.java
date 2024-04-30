c112698a2c97d10894fd8b7f93be552ec1711b38
Gavin_McClure <grm04@iastate.edu>
2024-04-30 17:46:46 -0500

    added calendar view holder

--- Frontend/frontmain/app/src/main/java/com/example/androidexample/CalendarViewHolder.java
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/CalendarViewHolder.java
@@ -1,14 +1,33 @@
 package com.example.androidexample;
 
-import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class CalendarViewHolder extends AppCompatActivity {
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    private final ArrayList<LocalDate> days;
+    public final View parentView;
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener, ArrayList<LocalDate> days)
+    {
+        super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+        this.days = days;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), days.get(getAdapterPosition()));
     }
 }
