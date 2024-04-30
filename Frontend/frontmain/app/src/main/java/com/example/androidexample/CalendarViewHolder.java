3dfb878ebe3b7f36eeef93c62110444f6f635e7f
Gavin_McClure <grm04@iastate.edu>
2024-04-30 17:25:50 -0500

    class added + xml edits

---
+++ Frontend/frontmain/app/src/main/java/com/example/androidexample/CalendarViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.androidexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarViewHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
